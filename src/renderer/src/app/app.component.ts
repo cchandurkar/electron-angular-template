bbfa64cbc1acd85b44e3ddc0b162952f7a601344
Chaitanya Chandurkar <cchandurkar@gmail.com>
2025-07-24 17:03:16 -0400

    Updagrade to Angular@19

--- src/renderer/src/app/app.component.ts
+++ src/renderer/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NgbNavModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [NgbNavModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'renderer';
